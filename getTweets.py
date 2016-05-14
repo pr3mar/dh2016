70f3944a90f6751785816a4ccbc98588c528145d
Marko Prelevic <markoprelevik@gmail.com>
2016-05-14 16:21:40 +0200

    tweets database

--- getTweets.py
+++ getTweets.py
@@ -1,5 +1,5 @@
-import twitter
-import json
+import twitter, json, sys, time
+import codecs
 
 consumer_key = "FBQBppfsDOSZqyTezocvvcmaH"
 consumer_secret = "SpVeH49YPu2qOsYFgc9idet1X60vHizqMZ1jldMvvRxQ62mJNJ"
@@ -8,14 +8,23 @@ acc_toke_secret = "HmXTkLmcrz4uZnVxkXAYLZhQhWZsJyj96Esm7AQo5iGeU"
 
 auth = twitter.OAuth(acc_token, acc_toke_secret, consumer_key, consumer_secret)
 twitter_api = twitter.Twitter(auth=auth)
-q = "@MLHacks"
-count = 1000
-sr = twitter_api.search.tweets(q=q, count=count, until="2016-05-15", result_type="mixed")
+q = "#dragonhack OR #dragonHack2016 OR @MLHacks"
+count = 10000
 
-print(len(sr["statuses"]))
-
-for tweet in sr["statuses"]:
-    print(tweet["text"])
+max_id = sys.maxsize
+retrieved = 0
+out_file = "tweets.txt"
+out_file = codecs.open(out_file, "a", "utf-8")
+while retrieved < count:
+    sr = twitter_api.search.tweets(q=q, count=count, max_id=max_id, result_type="mixed")
+    retrieved = retrieved + int(len(sr["statuses"]))
+    for tweet in sr["statuses"]:
+        if not tweet["truncated"]:
+            out_file.write(tweet["text"] + "\n")
+        if max_id > int(tweet["id"]):
+            max_id = int(tweet["id"])
+    time.sleep(5)
+print(retrieved)
 # print(json.dumps(sr, indent=1))
 
 #https://api.twitter.com/1.1/search/tweets.json?q
